81d0da7a6c0f2b781563c03d5a8b8600ab6ee635
Brendan Moran <b.daniel.moran@gmail.com>
2020-06-08 17:46:10 -0400

    staged and commited all files

---
+++ js/random.js
@@ -0,0 +1,45 @@
+function getRandom (lower, upper) {
+  "use strict";
+  
+  const lowNumber = parseInt(lower);
+  const highNumber = parseInt(upper);
+
+  if (lowNumber >= highNumber) {
+    
+    const err = new Error(`The value provided to "upper" was not greater than the value povided to "lower".`)
+    console.error(err);
+    return err;
+    
+  } else if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
+    
+    const randomNum = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
+    
+    return randomNum;
+    
+  } else {
+    
+    const err = new Error(` One of the arguments was not a number.`)
+    console.error(err)
+    return err;
+    
+    
+  }
+}
+
+//works:
+getRandom(3, 98);
+getRandom(-111, 0); 
+getRandom("-50", -1);
+getRandom(7, '21');
+
+//doesn't work:
+getRandom(52, 5);
+getRandom(7, 'George');
+getRandom('', 10);
+getRandom(10, '');
+getRandom('','');
+getRandom();
+
+
+
+
