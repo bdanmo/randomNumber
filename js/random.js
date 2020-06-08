05b9879bb59e1b7ffd965a3bfc4206306bd49d61
Brendan Moran <b.daniel.moran@gmail.com>
2020-06-08 19:29:47 -0400

    Change order of if/else if statements
    Change order of if/else if statements to test for success first

--- js/random.js
+++ js/random.js
@@ -4,24 +4,22 @@ function getRandom (lower, upper) {
   const lowNumber = parseInt(lower);
   const highNumber = parseInt(upper);
 
-  if (lowNumber >= highNumber) {
+  if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
+    
+    const randomNum = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
+    return randomNum;
+    
+  } else if (lowNumber >= highNumber) {
     
     const err = new Error(`The value provided to "upper" was not greater than the value povided to "lower".`)
     console.error(err);
     throw err; //or do whatever is best for your use case: e.g. provide user with an error message
-    
-  } else if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
-    
-    const randomNum = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
-    
-    return randomNum;
     
   } else {
     
     const err = new Error(` One of the arguments was not a number.`)
     console.error(err)
     throw err; // or do whatever is best for your use case
-    
     
   }
 }
