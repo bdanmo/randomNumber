9d5354988f367e249fda7519942ad4116f43f533
Brendan Moran <b.daniel.moran@gmail.com>
2020-06-08 19:35:05 -0400

    Undid the sucky changes of the last two commits.

--- js/random.js
+++ js/random.js
@@ -3,23 +3,24 @@ function getRandom (lower, upper) {
   
   const lowNumber = parseInt(lower);
   const highNumber = parseInt(upper);
-
-  if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
+  
+  if (lowNumber >= highNumber) {
+      
+      const err = new Error(`The value provided to "upper" was not greater than the value povided to "lower".`)
+      console.error(err);
+      //or do whatever is best for your use case: e.g. throw err, provide user with an error message
+  
+    
+  } else if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
     
     const randomNum = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
     return randomNum;
-    
-  } else if (lowNumber >= highNumber) {
-    
-    const err = new Error(`The value provided to "upper" was not greater than the value povided to "lower".`)
-    console.error(err);
-    throw err; //or do whatever is best for your use case: e.g. provide user with an error message
     
   } else {
     
     const err = new Error(` One of the arguments was not a number.`)
     console.error(err)
-    throw err; // or do whatever is best for your use case
+    // or do whatever is best for your use case: e.g. throw err, provide user with an error message
     
   }
 }
