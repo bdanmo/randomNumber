db84c9896de2769a0e4ee093a5e151511f8ecefd
Brendan Moran <b.daniel.moran@gmail.com>
2020-06-08 22:48:25 -0400

    Edit error messages to match new name of function.

---
+++ js/randomNumber.js
@@ -0,0 +1,26 @@
+function randomNumber (lower, upper) {
+  "use strict";
+  
+  const lowNumber = parseInt(lower);
+  const highNumber = parseInt(upper);
+  
+  if (lowNumber >= highNumber) {
+      
+      const err = new Error(`randomNumber: The number passed as the lower bound was greater than the number passed as the upper bound.`)
+      throw err;
+    
+  } else if (lowNumber && highNumber || lowNumber === 0 && highNumber || lowNumber && highNumber === 0) {  //allow for all possible combinations of negative and 0 values
+    
+    const randomNum = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
+    return randomNum;
+    
+  } else {
+    
+    const err = new Error(`randomNumber: One of the arguments provided was not a number.`)
+    throw err;
+    
+  }
+}
+
+
+
